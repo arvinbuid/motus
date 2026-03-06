b7d9627d8bfb6b19222f419e64e8ee9c48c295b7
Arvin Buid <arvinbuid01@gmail.com>
2026-03-06 20:43:44 +0800

    feat: setup application routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router"
+import Home from "./pages/Home"
+import Account from "./pages/Account"
+import Auth from "./pages/Auth"
+import Onboarding from "./pages/Onboarding"
+import Profile from "./pages/Profile"
+
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-semibold">Workout AI Planner</h1>
-    </div>)
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path='/account/:pathname' element={<Account />} />
+        <Route path='/auth/:pathname' element={<Auth />} />
+        <Route path='/onboarding' element={<Onboarding />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
